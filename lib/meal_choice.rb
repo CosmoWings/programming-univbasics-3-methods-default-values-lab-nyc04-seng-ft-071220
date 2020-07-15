2c9ec2a78a9ba46b20c90e10dcb20d354f39e253
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-15 19:45:31 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,11 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+require "pry"
 def meal_choice(veg,carb,meat = "tofu")
   puts "What a nutritious meal!"
   puts "A plate of #{meat} with #{veg} and #{carb}."
+  puts "Now I will call pry to help me debug"
+  binding.pry
 end
 
 meal_choice("carrots","rice")
